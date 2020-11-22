7f7ba06e08b6e0e5ec1c0f201a6a22697c6a6af2
Zaidanwar <48235316+Zaidanwar@users.noreply.github.com>
2020-11-22 12:39:33 +0530

    export books to imef file

---
+++ books.js
@@ -0,0 +1,11 @@
+export const books=[
+  { id:1,
+     author:'Amelia hepworth',
+ title:'i love u  to the  b and black',
+img:'https://m.media-amazon.com/images/I/61r-WomIz0L._AC_UY218_.jpg'},
+
+{id:2,
+ author:'jay shetty',
+ title:'Think like a monk',
+img:'https://images-eu.ssl-images-amazon.com/images/I/41gVhoPaE5L._AC_SX184_.jpg'},
+];
