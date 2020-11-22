7b3353f021a9f663fe71b67f355a890d43caa006
Zaidanwar <48235316+Zaidanwar@users.noreply.github.com>
2020-11-22 11:08:11 +0530

    this file contains iteration of arrays using map()

--- src/index.js
+++ src/index.js
@@ -4,37 +4,55 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
+const books=[
+  { id:1,
+     author:'Amelia hepworth',
+ title:'i love u  to the  b and black',
+img:'https://m.media-amazon.com/images/I/61r-WomIz0L._AC_UY218_.jpg'},
 
+{id:2,
+ author:'jay shetty',
+ title:'Think like a monk',
+img:'https://images-eu.ssl-images-amazon.com/images/I/41gVhoPaE5L._AC_SX184_.jpg'},
+];
 
 function Booklist()
 {
-  return (
+  return(
+  
     <section className='booklist'>
-    <Book/>
-    <Book/>
-    <Book/>
-    <Book/>
+      
+    
+      {books.map((book)=>{
+         const {img,title,author}=book;
+         return<Book key={book.id} book={book}></Book>;
+      })}
     </section>
-  )
 
-};
-const Book =()=>{
+  );
+
+}
+
+const Book =(props)=>{
+  const {img,title,author}=props.book;
   return(
     <article className='book'>
-    <Image></Image>
-    <Title></Title>
-    <Author></Author>
+    <img 
+  src={img}
+  alt=''/>
+    <h1>{title}</h1>
+    <h4>{author}</h4>
     </article>
   )
 };
-const Image=()=>(
-  <img 
-  src='https://m.media-amazon.com/images/I/61r-WomIz0L._AC_UY218_.jpg'
-  alt=''
-  />
-);
-const Title=()=> <h4>i love u to the and black</h4>;
-const Author=()=> <h4>Amelia hepworth</h4>;
+// const Image=()=>(
+//   <img 
+//   src='https://m.media-amazon.com/images/I/61r-WomIz0L._AC_UY218_.jpg'
+//   alt=''
+//   />
+// );
+// const Title=()=> <h4>i love u  to the  b and black</h4>;
+// const Author=()=> <h4>Amelia hepworth</h4>;
 
 ReactDOM.render(
   <Booklist></Booklist>,
